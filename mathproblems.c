b0fa67854c529f173720c749ebf21dce8dee80e6
fau20 <90728996+fau20@users.noreply.github.com>
2021-09-22 21:53:49 -0400

    Add files via upload

---
+++ mathproblems.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+// problem #1
+int multiples() {
+  int sum = 0;
+  int i = 0;
+  for(i = 0; i < 1000; i++) {
+    if(i % 3 == 0 || i % 5 == 0)
+    sum += i;
+  }
+  return sum;
+}
+// problem #6
+int sumSquare() {
+  int h = 0;
+  int i = 0;
+  int s = 0;
+  for(i = 1; i < 101; i++){
+    h += i;
+    s += (i * i);
+  }
+  return (h * h) - s;
+}
+int main() {
+  printf("The sum of all multiples of 3 or 5 below 1000 is %d", multiples());
+  printf("\nThe difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is %d", sumSquare());
+}
